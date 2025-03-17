b425c82058ae2a3d1d00289fef3fd1cc6aba7d12
gkvsanjeev <gkv.sanjeev@gmail.com>
2025-03-18 00:34:14 +0800

    Update app.js

--- app.js
+++ app.js
@@ -34,5 +34,5 @@ app.post("/getPromptResult", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
