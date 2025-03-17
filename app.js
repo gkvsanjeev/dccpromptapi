8a2aa9b2648922d4a8585acb8057f69c408f90a3
Sanjeev Kumar (NCS) <sanjeev.kumar2@ncs.com.sg>
2025-03-17 20:26:27 +0800

    Updated app.js, package.json, and added .gitignore

--- app.js
+++ app.js
@@ -34,5 +34,5 @@ app.post("/getPromptResult", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
